83e06a9f3e21cd9dcd8480cd3ca11ddbeeafabf2
Fagner Lima <contato@fagnerlima.pro.br>
2019-08-20 22:08:18 -0300

    Adiciona permissões de acesso para os Controllers

--- src/main/java/br/pro/fagnerlima/spring/auth/api/application/controller/GrupoController.java
+++ src/main/java/br/pro/fagnerlima/spring/auth/api/application/controller/GrupoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +26,12 @@ public class GrupoController {
     @Autowired
     private ResponseService responseService;
 
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_GRUPO_PESQUISAR') and #oauth2.hasScope('read')")
     @GetMapping
     public ResponseEntity<ResponseTO<Page<GrupoReducedResponseTO>>> findAll(Pageable pageable) {
         Page<Grupo> page = grupoService.findAll(pageable);
 
         return responseService.ok((new GrupoAssembler()).from(page));
     }
+
 }
--- src/main/java/br/pro/fagnerlima/spring/auth/api/infrastructure/security/configuration/OAuth2ResourceServerConfiguration.java
+++ src/main/java/br/pro/fagnerlima/spring/auth/api/infrastructure/security/configuration/OAuth2ResourceServerConfiguration.java
@@ -1,13 +1,18 @@
 package br.pro.fagnerlima.spring.auth.api.infrastructure.security.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
@@ -21,6 +26,11 @@ public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerA
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.stateless(true);
+    }
+
+    @Bean
+    public MethodSecurityExpressionHandler createMethodSecurityExpressionHandler() {
+        return new OAuth2MethodSecurityExpressionHandler();
     }
 
 }
