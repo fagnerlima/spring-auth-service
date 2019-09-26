81eea7f1c945a2f00a458c2452cf31bb44fabbf4
Fagner Lima <contato@fagnerlima.pro.br>
2019-09-26 08:00:02 -0300

    Adiciona configurações de Locale

--- src/main/java/br/pro/fagnerlima/spring/auth/api/application/configuration/ApplicationConfiguration.java
+++ src/main/java/br/pro/fagnerlima/spring/auth/api/application/configuration/ApplicationConfiguration.java
@@ -1,8 +1,12 @@
 package br.pro.fagnerlima.spring.auth.api.application.configuration;
 
+import java.util.Locale;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -10,6 +14,11 @@ public class ApplicationConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new FixedLocaleResolver(new Locale("pt", "BR"));
     }
 
 }
