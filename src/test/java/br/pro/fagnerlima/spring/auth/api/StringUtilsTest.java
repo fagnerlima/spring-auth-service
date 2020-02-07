2b3d02caffa977719ca095beafd6c9e63e18dd41
Fagner Lima <contato@fagnerlima.pro.br>
2020-02-07 16:00:06 -0300

    Adiciona testes de unidade

---
+++ src/test/java/br/pro/fagnerlima/spring/auth/api/StringUtilsTest.java
@@ -0,0 +1,18 @@
+package br.pro.fagnerlima.spring.auth.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import br.pro.fagnerlima.spring.auth.api.infrastructure.util.StringUtils;
+
+public class StringUtilsTest {
+
+    @Test
+    public void testUnaccent() {
+        assertThat(StringUtils.unaccent("Teste")).isEqualTo("Teste");
+        assertThat(StringUtils.unaccent("Salão de Recepção")).isEqualTo("Salao de Recepcao");
+        assertThat(StringUtils.unaccent("@#$%")).isEqualTo("@#$%");
+    }
+
+}
