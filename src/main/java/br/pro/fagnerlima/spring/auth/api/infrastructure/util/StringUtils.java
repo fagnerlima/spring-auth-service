40c1e683f353455f5b4de6d418fcd505292b6359
Fagner Lima <contato@fagnerlima.pro.br>
2020-02-17 08:02:16 -0300

    Adiciona configuração do JaCoCo

--- src/main/java/br/pro/fagnerlima/spring/auth/api/infrastructure/util/StringUtils.java
+++ src/main/java/br/pro/fagnerlima/spring/auth/api/infrastructure/util/StringUtils.java
@@ -4,6 +4,11 @@ import java.text.Normalizer;
 
 public class StringUtils {
 
+    /**
+     * Remove os acentos de uma string.
+     * @param str string a ser manipulada
+     * @return string sem acentos
+     */
     public static String unaccent(String str) {
         String normalizedStr = Normalizer.normalize(str, Normalizer.Form.NFD);
 
