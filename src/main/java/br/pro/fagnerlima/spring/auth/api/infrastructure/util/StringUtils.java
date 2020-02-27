189d31524ba11048b440ce029964a746aceb7035
Fagner Lima <contato@fagnerlima.pro.br>
2020-02-26 21:30:09 -0300

    Atualiza métodos de StringUtils

--- src/main/java/br/pro/fagnerlima/spring/auth/api/infrastructure/util/StringUtils.java
+++ src/main/java/br/pro/fagnerlima/spring/auth/api/infrastructure/util/StringUtils.java
@@ -1,36 +1,35 @@
 package br.pro.fagnerlima.spring.auth.api.infrastructure.util;
 
-import java.text.Normalizer;
-
 public class StringUtils {
 
     /**
      * Remove os acentos de uma string.
+     *
      * @param str string a ser manipulada
      * @return string sem acentos
      */
     public static String unaccent(String str) {
-        String normalizedStr = Normalizer.normalize(str, Normalizer.Form.NFD);
-
-        return normalizedStr.replaceAll("[^\\p{ASCII}]", "");
+        return org.apache.commons.lang3.StringUtils.stripAccents(str);
     }
 
     /**
      * Verifica se a string é vazia ou nula.
-     *
-     * <pre>
-     * StringUtils.isEmpty(null)      = true
-     * StringUtils.isEmpty("")        = true
-     * StringUtils.isEmpty(" ")       = false
-     * StringUtils.isEmpty("nome")     = false
-     * StringUtils.isEmpty("  nome  ") = false
-     * </pre>
      *
      * @param str string a ser verificada
      * @return {@code true} se a string for vazia ou nula
      */
     public static Boolean isEmpty(String str) {
         return org.apache.commons.lang3.StringUtils.isEmpty(str);
+    }
+
+    /**
+     * Verifica se a string é vazia, nula ou se contém apenas espaços em branco.
+     *
+     * @param str string a ser verificada
+     * @return {@code true} se a string for vazia, nula ou se contiver apenas espaços em branco
+     */
+    public static Boolean isBlank(String str) {
+        return org.apache.commons.lang3.StringUtils.isBlank(str);
     }
 
 }
--- src/test/java/br/pro/fagnerlima/spring/auth/api/infrastructure/util/StringUtilsTest.java
+++ src/test/java/br/pro/fagnerlima/spring/auth/api/infrastructure/util/StringUtilsTest.java
@@ -15,4 +15,20 @@ public class StringUtilsTest {
         assertThat(StringUtils.unaccent("@#$%")).isEqualTo("@#$%");
     }
 
+    @Test
+    public void testIsEmpty() {
+        assertThat(StringUtils.isEmpty("")).isTrue();
+        assertThat(StringUtils.isEmpty(null)).isTrue();
+        assertThat(StringUtils.isEmpty("  ")).isFalse();
+        assertThat(StringUtils.isEmpty(" a ")).isFalse();
+    }
+
+    @Test
+    public void testIsBlank() {
+        assertThat(StringUtils.isBlank("")).isTrue();
+        assertThat(StringUtils.isBlank(null)).isTrue();
+        assertThat(StringUtils.isBlank("  ")).isTrue();
+        assertThat(StringUtils.isBlank(" a ")).isFalse();
+    }
+
 }
