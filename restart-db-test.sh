b9c08b83b5f0974fc52f911195560bebcfdec3b5
Fagner Lima <contato@fagnerlima.pro.br>
2020-03-16 22:17:20 -0300

    Atualiza script restart-db-test

--- restart-db-test.sh
+++ restart-db-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker rm -f auth-db-test
 docker run --name auth-db-test -p 5432:5432 \
-  -e POSTGRES_DB=auth-test \
-  -e POSTGRES_USER=postgres \
-  -e POSTGRES_PASSWORD=postgres \
+  -e POSTGRES_DB=auth \
+  -e POSTGRES_USER=user.auth \
+  -e POSTGRES_PASSWORD=user.pass \
   -d postgres:10-alpine
