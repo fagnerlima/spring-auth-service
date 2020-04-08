1267fcc01eaf5d4fff1ba3bdc459e38785419378
Fagner Lima <contato@fagnerlima.pro.br>
2020-04-08 08:51:13 -0300

    Adiciona testes para GrupoController

--- src/test/java/br/pro/fagnerlima/spring/auth/api/application/controller/GrupoControllerTest.java
+++ src/test/java/br/pro/fagnerlima/spring/auth/api/application/controller/GrupoControllerTest.java
@@ -2,7 +2,6 @@ package br.pro.fagnerlima.spring.auth.api.application.controller;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -38,6 +37,7 @@ import br.pro.fagnerlima.spring.auth.api.test.auth.OAuth2AuthenticationTest;
 import br.pro.fagnerlima.spring.auth.api.test.util.ControllerTestUtils;
 import br.pro.fagnerlima.spring.auth.api.test.util.GrupoTestUtils;
 import br.pro.fagnerlima.spring.auth.api.test.util.PermissaoTestUtils;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 @ActiveProfiles("test")
@@ -52,7 +52,7 @@ public class GrupoControllerTest {
     private OAuth2Properties oauth2Properties;
 
     @LocalServerPort
-    private int port;
+    private Integer port;
 
     private String baseUrl;
 
@@ -63,7 +63,13 @@ public class GrupoControllerTest {
 
     private SpecificationFactory<Grupo> specificationFactoryMock;
 
+    List<Grupo> grupoListMock;
+
+    Page<Grupo> grupoPageMock;
+
     private Grupo grupoAdministradorMock;
+
+    private Grupo grupoTestMock;
 
     @Autowired
     public GrupoControllerTest(OAuth2Properties oauth2Properties) {
@@ -81,25 +87,26 @@ public class GrupoControllerTest {
         accessToken = OAuth2AuthenticationTest.givenAccessToken(baseUrl + TOKEN_PATH, oauth2Properties, ADMIN_USERNAME, ADMIN_PASSWORD);
         specificationFactoryMock = mock(SpecificationFactory.class);
 
-        Permissao permissaoAdmin = PermissaoTestUtils.createPermissao(1L, Papel.ROLE_ADMIN, "Administrador");
-        grupoAdministradorMock = GrupoTestUtils.createGrupo(1L, "Administrador", true, Set.of(permissaoAdmin));
+        Permissao permissaoAdminMock = PermissaoTestUtils.createPermissao(1L, Papel.ROLE_ADMIN, "Administrador");
+        grupoAdministradorMock = GrupoTestUtils.createGrupo(1L, "Administrador", true, Set.of(permissaoAdminMock));
+        grupoTestMock = GrupoTestUtils.createGrupo(21L, "Test", true, Set.of(permissaoAdminMock));
 
-        List<Grupo> gruposList = new ArrayList<>();
-        gruposList.add(grupoAdministradorMock);
+        grupoListMock = new ArrayList<>();
+        grupoListMock.add(grupoAdministradorMock);
 
         for (Long i = 2L; i <= 8; i++) {
-            gruposList.add(GrupoTestUtils.createGrupo(i, RandomStringUtils.random(10), true));
+            grupoListMock.add(GrupoTestUtils.createGrupo(i, RandomStringUtils.random(10), true));
         }
 
-        Page<Grupo> gruposPage = new PageImpl<>(gruposList, PageRequest.of(0, 10), 8);
+        grupoPageMock = new PageImpl<>(grupoListMock, PageRequest.of(0, 10), 8);
 
         when(specificationFactoryMock.create(any(), any(Class.class))).thenReturn(Specification.where(null));
-        when(grupoServiceMock.findAll(any())).thenReturn(gruposPage);
-        when(grupoServiceMock.findAll(any(), any())).thenReturn(gruposPage);
     }
 
     @Test
     public void testFindAll() {
+        when(grupoServiceMock.findAll(any(), any())).thenReturn(grupoPageMock);
+
         Response response = given()
                 .auth().oauth2(accessToken)
                 .when().get(baseUrl + BASE_PATH);
@@ -111,7 +118,114 @@ public class GrupoControllerTest {
                 .body("content[0].id", equalTo(grupoAdministradorMock.getId().intValue()))
                 .body("content[0].nome", equalTo(grupoAdministradorMock.getNome()))
                 .body("content[0].ativo", equalTo(grupoAdministradorMock.getAtivo()))
-                .body("content[0].links", hasSize(3));
+                .body("content[0].links.size()", equalTo(3));
+    }
+
+    @Test
+    public void testFindAll_whenUnauthorized() {
+        given()
+                .when().get(baseUrl + BASE_PATH)
+                .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value()).assertThat();
+    }
+
+    @Test
+    public void testFindById() {
+        when(grupoServiceMock.findById(any())).thenReturn(grupoAdministradorMock);
+
+        Response response = given()
+                .auth().oauth2(accessToken)
+                .when().get(baseUrl + BASE_PATH + "/" + 1);
+
+        response.then()
+                .statusCode(HttpStatus.OK.value()).assertThat();
+
+        assertGrupoResponseTO(response, grupoAdministradorMock);
+    }
+
+    @Test
+    public void testFindAllActive() {
+        when(grupoServiceMock.findAllActive()).thenReturn(grupoListMock);
+
+        Response response = given()
+                .auth().oauth2(accessToken)
+                .when().get(baseUrl + BASE_PATH + "/ativos");
+
+        response.then()
+                .statusCode(HttpStatus.OK.value()).assertThat()
+                .body("size()", equalTo(grupoListMock.size()))
+                .body("[0].id", equalTo(grupoAdministradorMock.getId().intValue()))
+                .body("[0].nome", equalTo(grupoAdministradorMock.getNome()));
+    }
+
+    @Test
+    public void testSave() {
+        when(grupoServiceMock.save(any())).thenReturn(grupoTestMock);
+
+        String requestBody = "{\n" + 
+                "  \"nome\": \"Test\",\n" + 
+                "  \"permissoes\": [1],\n" + 
+                "  \"ativo\": true\n" + 
+                "}";
+        Response response = given()
+                .auth().oauth2(accessToken)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when().post(baseUrl + BASE_PATH);
+
+        response.then()
+                .statusCode(HttpStatus.CREATED.value()).assertThat();
+
+        assertGrupoResponseTO(response, grupoTestMock);
+    }
+
+    @Test
+    public void testUpdate() {
+        when(grupoServiceMock.update(any(), any())).thenReturn(grupoTestMock);
+
+        String requestBody = "{\n" + 
+                "  \"nome\": \"Test\",\n" + 
+                "  \"permissoes\": [1],\n" + 
+                "  \"ativo\": true\n" + 
+                "}";
+        Response response = given()
+                .auth().oauth2(accessToken)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when().put(baseUrl + BASE_PATH + "/" + grupoTestMock.getId().intValue());
+
+        response.then()
+                .statusCode(HttpStatus.OK.value()).assertThat();
+
+        assertGrupoResponseTO(response, grupoTestMock);
+    }
+
+    @Test
+    public void testSwitchActive() {
+        when(grupoServiceMock.switchActive(any())).thenReturn(grupoTestMock);
+
+        Response response = given()
+                .auth().oauth2(accessToken)
+                .when().patch(baseUrl + BASE_PATH + "/" + grupoTestMock.getId().intValue() + "/ativo");
+
+        response.then()
+                .statusCode(HttpStatus.NO_CONTENT.value()).assertThat();
+    }
+
+    private void assertGrupoResponseTO(Response response, Grupo grupo) {
+        response.then()
+                .body("id", equalTo(grupo.getId().intValue()))
+                .body("nome", equalTo(grupo.getNome()))
+                .body("ativo", equalTo(grupo.getAtivo()));
+
+        List<Permissao> permissoes = new ArrayList<>(grupo.getPermissoes());
+
+        response.then()
+                .body("permissoes.size()", equalTo(permissoes.size()))
+                .body("permissoes[0].id", equalTo(permissoes.get(0).getId().intValue()))
+                .body("permissoes[0].papel", equalTo(permissoes.get(0).getPapel().name()))
+                .body("permissoes[0].descricao", equalTo(permissoes.get(0).getDescricao()))
+                .body("links.size()", equalTo(3));
     }
 
 }
